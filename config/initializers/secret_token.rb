5c2f858b61003279748a8856b7735194009994a5
Valentina Salas <valensalas@gmail.com>
2013-10-12 19:53:57 -0400

    Initialize Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GimlApp::Application.config.secret_key_base = '7e3a8410e3ae24273da8d6d089dae010179dd265f682923569b0d0989bf016c8a8cc210dcbe54400827a0751b7d68c8fa344e9c76a3fa310ba01087db6e0b9bc'
